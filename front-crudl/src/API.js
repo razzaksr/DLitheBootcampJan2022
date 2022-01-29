ec046acff31aad56e39511b5da32d48b80dadb54
razzaksr <razzaksr@gmail.com>
2022-01-29 12:28:08 +0530

    POC1 Completed

--- front-crudl/src/API.js
+++ front-crudl/src/API.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const url="http://localhost:8082/DLitheBootcampBasicPOC/rest"
+//const url="http://localhost:8082/DLitheBootcampBasicPOC/rest"
+const url="http://localhost:8080/DLitheBootcampBasicPOC-0.0.1-SNAPSHOT/rest"
 
 // axios.method(url,object)
 
